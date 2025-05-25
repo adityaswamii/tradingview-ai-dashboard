382765d631ac6db0c108b3205869600f417bc74d
adityaswamii <adityaswami2004@gmail.com>
2025-05-25 19:59:29 +0530

    imported streamlit wrapper for tradingview's lightweight charts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from streamlit_lightweight_charts import renderLightweightCharts
+
 
 st.title("Hello Streamlit")
 
 if st.button("Send balloons!"):
     st.balloons()
-    
+
+
+# renderLightweightCharts(charts: <List of Dicts> , key: <str>)
+
